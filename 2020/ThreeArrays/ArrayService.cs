edade9739e6dab55b8bf60266d994b7fc47fbe0c
Ronald Cabrera González <ronald.cabrera.gonzalez@gmail.com>
2024-04-02 21:22:22 -0400

    Added ThreeArrays
    This one was pretty much figuring out how arrays work exactly, I quite liked the definition from back then, the teacher explained it like a soda crate.

---
+++ 2020/ThreeArrays/ArrayService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeArrays
+{
+    public static class ArrayService
+    {
+        public static int[] SetNumbersUntilTarget(int target)
+        {
+            int[] results = new int[target];
+
+            for (int i = 0; i < target; i++)
+            {
+                Console.WriteLine("Input a Number:");
+                _ = int.TryParse(Console.ReadLine(), out results[i]);
+            }
+
+            return results;
+        }
+
+        public static void SumArrayValues(int[] integers)
+        {
+            int sum = integers.Sum();
+            Console.WriteLine($"The sum of this array equals to: {sum}");
+        }
+
+        public static void ArrayContainsTargetValue(int[] integers, int target)
+        {
+            var sevenMessage = integers.Contains(target) 
+                ? "Hello, this int array does contain the number seven."
+                : "Goodbye, this int array does NOT contain the number seven.";
+
+            Console.WriteLine($"{sevenMessage}");
+        }
+
+        public static void SumEvenOddArrayValues(int[] integers)
+        {
+            int evenNumbers = integers.Where(c => c % 2 == 0).Count();
+            int oddNumbers = integers.Where(c => c % 2 != 0).Count();
+
+            Console.WriteLine($"This array contains: {evenNumbers} even numbers and {oddNumbers} odd numbers");
+        }
+    }
+}
---
+++ 2020/ThreeArrays/Program.cs
@@ -0,0 +1,10 @@
+﻿using ThreeArrays;
+
+var intArray = ArrayService.SetNumbersUntilTarget(5);
+ArrayService.SumArrayValues(intArray);
+
+intArray = ArrayService.SetNumbersUntilTarget(7);
+ArrayService.ArrayContainsTargetValue(intArray, 23);
+
+intArray = ArrayService.SetNumbersUntilTarget(10);
+ArrayService.SumEvenOddArrayValues(intArray);

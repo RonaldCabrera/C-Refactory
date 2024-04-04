7b60fc109ca6b9fa8e5c74bd8af660d25c082a62
Ronald Cabrera González <ronald.cabrera.gonzalez@gmail.com>
2024-04-03 20:09:09 -0400

    Optimized the CurrencyExchange main code.
    Thanks to my Sensei, made some further optimizations on the code.

--- 2020/CurrencyExchange/Program.cs
+++ 2020/CurrencyExchange/Program.cs
@@ -1,20 +1,21 @@
 ﻿using CurrencyExchange;
-using System.Text.Json;
+using System.Net.Http.Json;
 
 const string url = "https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/USD";
 using (var webClient = new HttpClient())
 {
-    var json = await webClient.GetStreamAsync(url);
-    var currency = JsonSerializer.Deserialize<CurrencyModel>(json);
-
-    if(currency is not null)
+    var currency = await webClient.GetFromJsonAsync<CurrencyModel>(url);
+    if(currency is null)
     {
-        double exchangeRate = currency.conversion_rates.DOP;
-
-        Console.WriteLine("Howdy, how many dollars you got?");
-        _ = double.TryParse(Console.ReadLine(), out double usdAmount);
-        if (usdAmount == 0) return;
-
-        Console.WriteLine($"Woah, that's the equivalent to {usdAmount * exchangeRate} $DOP.");
+        Console.WriteLine($"Something went wrong while requesting the currency data.");
+        return;
     }
+
+    double exchangeRate = currency.conversion_rates.DOP;
+
+    Console.WriteLine("Howdy, how many dollars you got?");
+    _ = double.TryParse(Console.ReadLine(), out double usdAmount);
+    if (usdAmount == 0) return;
+
+    Console.WriteLine($"Woah, that's the equivalent to {usdAmount * exchangeRate} $DOP.");
 }
